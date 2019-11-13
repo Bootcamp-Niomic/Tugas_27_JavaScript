2f6f7cbddf73913806634361f3ed3fc802bc6289
Havid <great_havid@yahoo.co.id>
2019-11-13 11:48:25 +0700

    Tugas 27 JS

---
+++ tugas27.js
@@ -0,0 +1,14 @@
+// ====== Tugas 27 JavaScript =====
+
+addrs1 = "No";
+addrs2 = " Rumah saya "
+addrs3 = 20;
+addrs4 = " Jalan Kaliurang";
+addrs5 = "KM";
+addrs6 = 15;
+
+var data1 = addrs1 + addrs2 + String(addrs3) + addrs4;
+var data2 = addrs5 + " " + String(addrs6)
+
+console.log(data1)
+console.log(data2);
